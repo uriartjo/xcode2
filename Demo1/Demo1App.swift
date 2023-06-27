8d0763006da55a48139cfaaba804c58800a8a51b
Joey <joey.uriarte@saintandrews.net>
2023-06-27 14:13:14 -0400

    Initial Commit

---
+++ Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by Joseph Uriarte on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
